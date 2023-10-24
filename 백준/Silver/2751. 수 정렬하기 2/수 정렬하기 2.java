bd4df27b9af9cb9a61fc540eff8ade3ae76c7acb
Shin Yukyung <96154444+shin-6-0@users.noreply.github.com>
2023-10-24 14:25:39 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1496 ms, Memory: 130624 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        // list 계열 중 하나를 쓰면 된다.
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list); //Arrays.sort대신 Collection에 있는 sort를 사용
+
+        for(int value : list) {
+            sb.append(value).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+}
